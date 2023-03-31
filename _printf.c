587959432f5137fd59b170b176853e1ad841473a
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-03-31 02:32:00 +0100

    _printf function

--- _printf.c
+++ _printf.c
@@ -1,126 +1,50 @@
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#define BUFF_SIZE 1024
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
- * struct buffer_s - buffer structure
- * @buffer: buffer array
- * @start: starting index of buffer to write from
- * @index: current index of buffer to write to
- * @total: total characters written to buffer
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
-typedef struct buffer_s
-{
-    char buffer[BUFF_SIZE];
-    int start;
-    int index;
-    int total;
-} buffer_t;
-
-int _printf(const char *format, ...);
-buffer_t *init_buffer(void);
-void write_buffer(buffer_t *output);
-void free_buffer(buffer_t *output);
-int _putchar(char c);
-int print_char(buffer_t *output, char c);
-int print_string(buffer_t *output, char *str);
-int print_int(buffer_t *output, int n);
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
-int handle_print(const char *format, int *i, va_list list, buffer_t *output, int flags, int width, int precision, int size);
-
 int _printf(const char *format, ...)
 {
-    va_list list;
-    buffer_t *output;
-    int i, printed, flags, width, precision, size;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-    if (format == NULL)
-        return (-1);
+	register int count = 0;
 
-    output = init_buffer();
-    if (output == NULL)
-        return (-1);
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 
-    va_start(list, format);
-
-    for (i = 0, printed = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            flags = get_flags(format, &i);
-            width = get_width(format, &i, list);
-            precision = get_precision(format, &i, list);
-            size = get_size(format, &i);
-            printed += handle_print(format, &i, list, output, flags, width, precision, size);
-        }
-        else
-        {
-            printed += print_char(output, format[i]);
-        }
-    }
-
-    va_end(list);
-
-    write_buffer(output);
-    free_buffer(output);
-
-    return (printed);
 }
-
-buffer_t *init_buffer(void)
-{
-    buffer_t *output;
-
-    output = malloc(sizeof(buffer_t));
-    if (output == NULL)
-        return (NULL);
-
-    output->start = 0;
-    output->index = 0;
-    output->total = 0;
-
-    return (output);
-}
-
-void write_buffer(buffer_t *output)
-{
-    if (output == NULL || output->index == 0)
-        return;
-
-    write(1, output->buffer + output->start, output->index - output->start);
-    output->total += output->index - output->start;
-    output->start = output->index;
-}
-
-void free_buffer(buffer_t *output)
-{
-    free(output);
-}
-
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-
-int print_char(buffer_t *output, char c)
-{
-    if (output == NULL)
-        return (-1);
-
-    if (output->index == BUFF_SIZE)
-        write_buffer(output);
-
-    output->buffer[output->index++] = c;
-
-    return (1);
-}
-
-int print_string(buffer_t *output, char *str)
-{
-    int i, len
-
